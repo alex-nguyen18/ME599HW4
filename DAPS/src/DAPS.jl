5d415d5d568df132e1f3001dc0859a86794c43fc
alex-nguyen18 <atng223@uky.edu>
2020-02-26 00:03:11 -0500

    Setting up helper functions; suggesting a solution to dist()

--- DAPS/src/DAPS.jl
+++ DAPS/src/DAPS.jl
@@ -8,4 +8,20 @@ function RRT(s,g,O)
 
 end
 
+function addPoints()
+
+end
+
+# Fairly straight forward
+function dist(f1,f2)
+    p1 = f1[1:3,4]
+    p2 = hcat(f2[1:3,4])
+    return (p2*p1)^.5
+end
+
+# I think we could just do total distance here
+function pathcost(f)
+
+end
+
 end # module
