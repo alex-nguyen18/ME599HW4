ea46ee7318df2c95f4000fa6760c0149fbf268b8
alex-nguyen18 <atng223@uky.edu>
2020-02-26 13:47:41 -0500

    Added pathcost function; simple sum of dist() calls

--- DAPS/src/DAPS.jl
+++ DAPS/src/DAPS.jl
@@ -1,27 +1,36 @@
 module DAPS
+#I just took the first letter of each of our names
 
+# Prob. Road Map
 function PRM(s,g,O)
 
 end
 
+# Randomly-exploring Random Tree
 function RRT(s,g,O)
 
 end
 
+#
 function addPoints()
 
 end
 
-# Fairly straight forward
+# Fairly straight forward; dot product and sqrt it
 function dist(f1,f2)
     p1 = f1[1:3,4]
     p2 = hcat(f2[1:3,4])
-    return (p2*p1)^.5
+    return ((p2*p1)[1])^.5
 end
 
 # I think we could just do total distance here
+# Assume that frames include the target frame too
 function pathcost(f)
-
+    sum = 0.0 #needs to be a float at least
+    for i = 1:size(f,1)
+        sum += dist(f[i].f[i+1])
+    end
+    return sum
 end
 
 end # module
