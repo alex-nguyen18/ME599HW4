5f19c48d7097d0f182eaa0d7c5f54462c0442ea6
alex-nguyen18 <atng223@uky.edu>
2020-02-27 14:28:59 -0500

    Change frame from h-matrix to position vector

--- DAPS/src/DAPS.jl
+++ DAPS/src/DAPS.jl
@@ -8,6 +8,8 @@ using StaticArrays
 const maxN = 1000
 const radCluster = 10
     
+# frame are assumed to be 3x1 vectors [x; y; z]
+
 function norm3(f,S)
     return ((f[1]-S[1])^2+(f[2]-S[2])^2+(f[3]-S[3])^2)^.5
 end
@@ -17,17 +19,17 @@ function norm2(f,C)
 end
 
 function checkFrame(f,O)
-    if f[3,4] < 0
+    if f[3] < 1
     for i = 1:size(O,1) 
         # Check if cylinder or sphere
-        if O[i][5] == 1
+        if O[i,5] == 1
             # Check if it is outside of cylinder space (Over estimate the cylinder)
-            if (f[3][4] < O[i][3] + 1 && O[i][4] + 1 < norm2([f[1,4] f[2,4]],O[i]))
+            if (f[3] < O[i,3] + 1 && O[i,4] + 1 < norm2(f,O[i]))
                 return false
             end
         else
             # Check if it is outside sphere space
-            if O[i][4] + 1 < norm3([f[1,4] f[2,4] f[3,4]],O[i]) 
+            if O[i,4] + 1 < norm3(f,O[i]) 
                 return false
             end
         end
@@ -36,29 +38,35 @@ function checkFrame(f,O)
 end
 
 # Homogenous Trans. Matrix of X-Y-Z euler rotation
-function eulerRotationMatrix(theta1,theta2,theta3)
-	Rx = [1 0 0 0; 0 cos(theta1) -sin(theta1) 0; 0 sin(theta1) cos(theta1) 0; 0 0 0 1]
-	Ry = [cos(theta2) 0 sin(theta2) 0; 0 1 0 0; -sin(theta2) 0 cos(theta2) 0; 0 0 0 1]
-	Rz = [cos(theta3) -sin(theta3) 0 0; sin(theta3) cos(theta3) 0 0; 0 0 1 0; 0 0 0 1]
-	return Rx*Ry*Rz
-end
+#function eulerRotationMatrix(theta1,theta2,theta3)
+#	Rx = [1 0 0 0; 0 cos(theta1) -sin(theta1) 0; 0 sin(theta1) cos(theta1) 0; 0 0 0 1]
+#	Ry = [cos(theta2) 0 sin(theta2) 0; 0 1 0 0; -sin(theta2) 0 cos(theta2) 0; 0 0 0 1]
+#	Rz = [cos(theta3) -sin(theta3) 0 0; sin(theta3) cos(theta3) 0 0; 0 0 1 0; 0 0 0 1]
+#	return Rx*Ry*Rz
+#end
 
 # Add a new random point/configuration
-# Consider expanding off of a current point within a certain radius?
+# Add a random point within a 10-edge length cube around the point of interest
+# .1 unit increments
 function addPoint(p)
-    r = rand(collect(1:radCluster))
-    trans = p + [0 0 0 0; 0 0 0 0; 0 0 0 r; 0 0 0 0]
-    rAng = rand(3)
-    rRot = eulerRotationMatrix(rAng[1],rAng[2],rAng[3])
-    return trans*rRot
+    dx = rand(LinRange(-5, 5, 110))
+    dy = rand(LinRange(-5, 5, 110))
+    dz = rand(LinRange(-5, 5, 110))
+    return [p[1]+dx;p[2]+dy;p[3]+dz]
+    #r = rand(collect(1:radCluster))
+    #trans = p + [0 0 0 0; 0 0 0 0; 0 0 0 r; 0 0 0 0]
+    #rAng = rand(3)
+    #rRot = eulerRotationMatrix(rAng[1],rAng[2],rAng[3])
+    #return trans*rRot
 end
 
-function getPos(f)
-    return [0 0 0 f[1,4]; 0 0 0 f[2,4]; 0 0 0 f[3,4]; 0 0 0 0]
-end
+#function getPos(f)
+#    return [0 0 0 f[1,4]; 0 0 0 f[2,4]; 0 0 0 f[3,4]; 0 0 0 0]
+#end
+
 # check if edge is realistic (I was thinking 10% increments)
 function checkEdge(f1,f2,O)
-    v = getPos(f2) - getPos(f1)
+    v = f2-f1
     for i = 1:10
         p = f1 + (i/10)(v)
         if !checkFrame(p,O)
@@ -82,13 +90,14 @@ function PRM(s,g,O)
     start_point = Vec3f(s[1:3,4])
     # read the goal from the input
     goal_point = Vec3f(g[1:3,4])
-    nodes = cat(start_point,goal_point; dims=3) # stack horizontally because they are naturally vertical
+    nodes = cat(goal_point, start_point; dims=2) # stack horizontally because they are naturally vertical
     numNodes = 2
     edges = []
     while (numNodes < maxN)
-        index = rand(collect(1:numNodes))
+        # don't allow the finish to grow a node
+        index = rand(collect(2:numNodes))
         # add a new frame
-        newPoint = addPoint(nodes[:,:,index])
+        newPoint = addPoint(nodes[:,index])
         # check if new point is in
         if checkFrame(newPoint,O)
             # add node position if it passes
@@ -97,9 +106,9 @@ function PRM(s,g,O)
             for i = 1:size(nodes,3)
                 # if this is within an acceptable radius, then add the node
                 if norm3([newPoint[1,4] newPoint[2,4] newPoint[3,4]],[nodes[i][1,4] nodes[i][2,4] nodes[i][3,4]]) < radCluster
-                    if checkEdge(newPoint,O[:,:,i],O)
+                    if checkEdge(newPoint,nodes[:,i],O)
                         edges = [edges; numNodes+1 i]
-                        if i == 2
+                        if i == 1
                             path = findpath(nodes, edges)
                         end
                     end
