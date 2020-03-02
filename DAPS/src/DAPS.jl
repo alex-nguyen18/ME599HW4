96b4b03087a8ec91d5df24a1c058ce45d542a9c7
Derek <61194812+daka228@users.noreply.github.com>
2020-03-02 17:56:58 -0500

    Update DAPS.jl

--- DAPS/src/DAPS.jl
+++ DAPS/src/DAPS.jl
@@ -1,14 +1,64 @@
 module DAPS
 #I just took the first letter of each of our names
 
+
+function ccw(A,B,C)
+    val = (C(2)-A(2)) * (B(1)-A(1)) > (B(2)-A(2)) * (C(1)-A(1));
+end # ccw
+
+function noCollision(n2, n1, o)
+    A = [n1(1) n1(2)];
+    B = [n2(1) n2(2)];
+    obs = [o(1) o(2) o(1)+o(3) o(2)+o(4)];
+    
+    C1 = [obs(1),obs(2)];
+    D1 = [obs(1),obs(4)];
+    C2 = [obs(1),obs(2)];
+    D2 = [obs(3),obs(2)];
+    C3 = [obs(3),obs(4)];
+    D3 = [obs(3),obs(2)];
+    C4 = [obs(3),obs(4)];
+    D4 = [obs(1),obs(4)];
+    
+    # Check if path from n1 to n2 intersects any of the four edges of the
+    # obstacle
+    
+    ints1 = ccw(A,C1,D1) != ccw(B,C1,D1) && ccw(A,B,C1) != ccw(A,B,D1); 
+    ints2 = ccw(A,C2,D2) != ccw(B,C2,D2) && ccw(A,B,C2) != ccw(A,B,D2);
+    ints3 = ccw(A,C3,D3) != ccw(B,C3,D3) && ccw(A,B,C3) != ccw(A,B,D3);
+    ints4 = ccw(A,C4,D4) != ccw(B,C4,D4) && ccw(A,B,C4) != ccw(A,B,D4);
+    if ints1==0 && ints2==0 && ints3==0 && ints4==0
+        nc = 1;
+    else
+        nc = 0;
+    end
+end # noCollision
+
+# steer towards qn with maximum step size of eps
+function steer(qr, qn, val, eps)
+    qnew = [0,0];
+    if val >= eps
+        qnew(1) = qn(1) + ((qr(1)-qn(1))*eps)/dist(qr, qn);
+        qnew(2) = qn(2) + ((qr(2)-qn(2))*eps)/dist(qr, qn);
+        qnew(3) = qn(3) + ((qr(3)-qn(3))*eps)/dist(qr, qn);
+    else
+        qnew(1) = qr(1);
+        qnew(2) = qr(2);
+        qnew(3) = qr(3);
+    end
+    A = [qnew(1), qnew(2), qnew(3)];
+end # steer
+
 # Prob. Road Map
 function PRM(s,g,O)
 
 end
 
+
+
 # Randomly-exploring Random Tree
 function RRT(s,g,O)
-    
+   
     const Vec4f = SVector{4, Float64}
 
     # read the starting point from the input
@@ -16,40 +66,84 @@ function RRT(s,g,O)
     # read the goal from the input
     goal_point = Vec4f(g)
 
-    # add the floor as an obstacle, infinite radius circle at origin
-    floor = [0 0 0 Base.Inf 0]
-    # adding the floor to the obstacle matrix
-    O = [O;floor]
+    nodes = cat(start_point, goal_point; dims=3)
+    numNodes = 2
+    RRT_edges =[]
+    EPS = 15
 
-    # Read the total number of obstacles from the input + floor
-    O_number_rows = size(O)[1]
-    
-    # Draw obstacles
-    for i = 1:1:O_number_rows # iterate through all obstacle inputs
-        if O[i,5] = 1 # check if the obstacle is a cylinder
-              x_coordinate = O[i,1] # read the x coordinate of cylinder position
-              y_coordinate = O[i,2] # read the y coordinate of cylinder position
-              height_cylinder = O[i,3] # read the height of the cylinder
-              radius_cylinder = O[i,4] # read the radius of the cylinder
-            function cylinder # draw cylinder obstacle using coordinates and height
-              # creates the geometry of the cylinder through 2 circles at varius heights and a common radius
-                geom = Cylinder([x_coordinate; y_coordinate; z_coordinate], radius_cylinder, height_cylinder)
-            end # end cylinder function
-        else # if not a cylinder, use sphere
-              x_coordinate = O[i,1] # read the x coordinate of sphere position
-              y_coordinate = O[i,2] # read the y coordinate of sphere position
-              z_coordinate = O[i,3] # read the z coordinate of sphere position
-              radius_sphere = O[i,4] # read the radius of the sphere
-            function sphere # draw sphere obstacle with coordinates and radius
-            # creates the geometry of the sphere through origin and radius
-                geom = Sphere([x_coordinate; y_coordinate; z_coordinate],radius_sphere)
-            end # end sphere function
-            
-        end
-        
-    end 
-    
-end
+    # add the floor as an obstacle, infinite radius circle at origin
+    floor_obstacle = [0 0 0 Base.Inf 0]
+    # adding the floor to the obstacle matrix
+    Obstacles = [O;floor_obstacle]
+
+    for i = 1:1:maxN
+        q_rand = [floor(rand(1)*g[1,1]); floor*rand(1)*g[1,2]; floor(rand(1)*g[1,3]];
+        # pick the closest node from existing list to branch out from
+        ndist = []
+        for j = 1:1:length(nodes)
+            n = nodes(j)
+            tmp = dist(n.coordinates, q_rand)
+            ndist = [ndist tmp]
+        end 
+
+        [val, idx] = min(ndist)
+        q_near = nodes(idx)
+
+        q_new_coordinates = steer(q_rand, q_near_coordinates, val, EPS);
+        if noCollision(q_rand, q_new_coordinates, Obstacles)
+            q_new_cost = dist(q_new_coordinates, q_near.coordinates) + q_near.cost;
+
+            # Within radius, find all existing nodes
+            q_nearest = []
+            r = 60;
+            neighbor_count = 1;
+            for j = 1:1:length(nodes)
+                if noCollision(nodes(j.coordinates), q_new_coordinates, O) && dist(nodes(j).coord, q_new.coord) <= r
+                    q_nearest(neighbor_count).coordinates = nodes(j).coordinates;
+                    q_nearest(neighbor_count).cost = nodes(j).cost;
+                    neighbor_count = neighbor_count + 1;
+                end
+            end
+
+             # initialize cost to currently known value
+             q_min = q_near;
+             C_min = q_new_cost;
+
+              # iterate through all nearest neighbors to find alternate lowest cost paths
+            for k = 1:1:length(q_nearest)
+                if noCollision(q_nearest(k).coord, q_new.coord, O) && q_nearest(k).cost + dist(q_nearest(k).coord, q_new.coord) < C_min
+                    q_min = q_nearest(k);
+                    C_min = q_nearest(k).cost + dist(q_nearest(k).coord, q_new.coord);            
+                end
+            end
+
+            # update parent to least cost-from node
+            for j = 1:1:length(nodes)
+                if nodes(j).coordinates == q_min.coordinates
+                    q_new.parent = j;
+                end
+            end
+
+            # append to nodes
+            nodes = [nodes; q_new_coordinates]
+        end 
+    end
+    D = []
+    for j = 1:1:length(nodes)
+        tmpdist = dist(nodes(j).coordinates, goal_point.coordinates);
+        D = [D tmpdist];
+    end
+
+    # Search backwards from goal to start to find the optimal path
+    [val, idx] = min(D);
+    q_final = nodes(idx)
+    goal_point.parent = idx;
+    q_end = goal_point;
+    nodes = [nodes goal_point];
+    while q_end.parent != 0
+        start = q_end.parent;
+        q_end = nodes(start);
+                    end # end RRT
 
 #
 function addPoints()
